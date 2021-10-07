fc25811276b2504a0975c16590fc5fad9630525d
jueleejue <jueleejue@gmail.com>
2021-10-07 15:39:50 +0900

    first commit

---
+++ src/main/java/se/authserver/SeAuthServerApplication.java
@@ -0,0 +1,13 @@
+package se.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeAuthServerApplication.class, args);
+	}
+
+}
