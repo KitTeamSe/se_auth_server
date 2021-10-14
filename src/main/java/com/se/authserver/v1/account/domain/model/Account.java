e1e236d7d75c3a617c8a1be45125f12e1da8b6fd
jueleejue <jueleejue@gmail.com>
2021-10-14 15:50:28 +0900

    Account Entity 작성

--- src/main/java/com/se/authserver/v1/account/domain/model/Account.java
+++ src/main/java/com/se/authserver/v1/account/domain/model/Account.java
@@ -1,9 +1,12 @@
 package com.se.authserver.v1.account.domain.model;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import com.se.authserver.v1.common.domain.model.BaseEntity;
+import javax.validation.constraints.Email;
 
 @Entity
 public class Account extends BaseEntity {
@@ -12,6 +15,35 @@ public class Account extends BaseEntity {
   @GeneratedValue
   private Long accountId;
 
-  // TODO 필요한 속성 추가
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false, unique = true)
+  private String identifier;  //uuid
+
+  @Column
+  private String phone;
+
+  @Email
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private LocalDateTime birth;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Country country;
+
+  @Column
+  private String address;
+
+  @Column
+  private String studentId;
+
+  @Column(unique = true)
+  private String authorizedEmail;
 
 }
