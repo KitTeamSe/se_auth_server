9489a0e1af65917bea70287514398faddf410db2
jueleejue <jueleejue@gmail.com>
2021-10-21 17:25:44 +0900

    fix : cancel presenter

--- src/main/java/com/se/authserver/v1/account/presentation/presenter/AccountPresenterFormatter.java
+++ src/main/java/com/se/authserver/v1/account/presentation/presenter/AccountPresenterFormatter.java
@@ -1,6 +1,6 @@
 package com.se.authserver.v1.account.presentation.presenter;
 
-import com.se.authserver.v1.account.application.dto.AccountReadDto;
+//import com.se.authserver.v1.account.application.dto.AccountReadDto;
 import com.se.authserver.v1.common.presentation.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -18,9 +18,9 @@ public class AccountPresenterFormatter implements AccountPresenter{
     return new Response(HttpStatus.OK, "로그인 되었습니다", token);
   }
 
-  @Override
-  public Response<AccountReadDto> read(AccountReadDto accountReadDto) {
-    return new Response(HttpStatus.OK, "회원 단일 조회 성공하였습니다", accountReadDto);
-  }
+//  @Override
+//  public Response<AccountReadDto> read(AccountReadDto accountReadDto) {
+//    return new Response(HttpStatus.OK, "회원 단일 조회 성공하였습니다", accountReadDto);
+//  }
 
 }
